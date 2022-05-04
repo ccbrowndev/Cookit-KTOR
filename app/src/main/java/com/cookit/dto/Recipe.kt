4bff51bf1a6e22e484a2d40c6d817520dd3b784d
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-05-04 13:38:20 -0400

    add annotations to Recipe for use by Room

--- app/src/main/java/com/cookit/dto/Recipe.kt
+++ app/src/main/java/com/cookit/dto/Recipe.kt
@@ -1,19 +1,24 @@
 package com.cookit.dto
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "recipe")
 data class Recipe(
-    @SerializedName("idMeal")var recipeID : String = "",
-    @SerializedName("strMeal")var name : String = "",
-    @SerializedName("strCategory")var category : String = "",
-    @SerializedName("strArea")var cuisine : String = "",
-    @SerializedName("strInstructions")var instructions : String = "",
-    @SerializedName("strMealThumb")var imageURL : String = "",
-    @SerializedName("strYoutube")var youtubeURL : String = "",
-    @Expose var ingredients : MutableMap<String, String> = LinkedHashMap(),
-    @Transient var fireStoreID: String = "",
-    ) {
+    @PrimaryKey @SerializedName("idMeal") var recipeID: String = "",
+    @SerializedName("strMeal") var name: String = "",
+    @SerializedName("strCategory") var category: String = "",
+    @SerializedName("strArea") var cuisine: String = "",
+    @SerializedName("strInstructions") var instructions: String = "",
+    @ColumnInfo(name = "image_url") @SerializedName("strMealThumb") var imageURL: String = "",
+    @ColumnInfo(name = "youtube_url") @SerializedName("strYoutube") var youtubeURL: String = "",
+    @Expose var ingredients: MutableMap<String, String> = LinkedHashMap(),
+    @Ignore @Transient var fireStoreID: String = "",
+) {
     override fun toString(): String {
         return name
     }
